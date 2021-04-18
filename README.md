cfd2183a284de8928b6248ad994bad46f8f92446
linuxkidvoid <jgao@ntst.com>
2021-04-18 14:49:05 -0400

    finish sorting tests

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # saucedemo
+
+**Pre requirements:**
+Make sure to have git installed already (https://git-scm.com/download)
+Make sure to have node and npm installed already (https://nodejs.org/en/download/)
+
+**Getting the contents:**
+Clone this repo
+> git clone https://github.com/linuxkidvoid/saucedemo.git
+
+**Install packages:**
+Type below command from the root of the folder
+> npm install
+
+**Run in cypress GUI:** 
+Run command from root of the folder and select either all tests or an individual test to run
+
+> npx cypress open (if you have npx install already)
+
+  or
+
+> npm run cypress open
+
+
+**Some design considerations:**
+- When locating dom elements, try to use 'data-test' attribute when possible for future front-end change proof.
+- Each tests are independent of each other for stability and parallellation.
+- Page Objects pattern will make locator and method re-use and makes maintainess easier.
+- Usually, username and password information are to store in a .env type of file and not to pushed to repo, however, those are public information on demo site in this case.  
--- cypress/integration/product_page.spec.js
+++ cypress/integration/product_page.spec.js
@@ -1,19 +1,15 @@
 import ProductPage from '../page_objects/ProductPage'
 
-// don't forget to test multiple browsers in concurrent
-// don't forget to add a runner
-// don't forget to create script in package.json
-
 describe('Product', () => {
   const productPage = new ProductPage()
 
   beforeEach(() => {
     cy.visit('/')
     cy.loginAsStandard()
+    cy.fixture('products.json').as('productsJSON')
   })
 
   describe('Collapsable menu opens and its selections are present', () => {
-
     it('collapsable menu opens and its selections are present', () => {
 
       cy.get('#react-burger-menu-btn').click()
@@ -24,24 +20,99 @@ describe('Product', () => {
     })
   })
 
-  describe('Sorting options for products properly sort all products', () => {
+  describe('Sorting options for products page', () => {
+    it('can sort by Name (A-Z)', async () => {
+      const productsJSON = await cy.get('@productsJSON') 
+      let itemNames = await getItemNames(productsJSON)
 
-    it('sort by Name (A-Z)', () => {
+      // expected order
+      itemNames.sort()
+
+      // sort action
       productPage.sortByNameAZ()
+
+      // verify the names order
+      verifyNamesOrder(itemNames)
     })
 
-    it('sort by Name (Z-A)', () => {
+    it('can sort by Name (Z-A)', async () => {
+      const productsJSON = await cy.get('@productsJSON') 
+      let itemNames = await getItemNames(productsJSON)
+
+      // expected order
+      itemNames.reverse()
+
+      // sort action
       productPage.sortByNameZA()
+
+      // verify the names order
+      verifyNamesOrder(itemNames)
     })
 
-    it('sort by Price (low to high)', () => {
+    it('can sort by Price (low to high)', async () => {
+      const productsJSON = await cy.get('@productsJSON') 
+      let itemPrices = await getItemPrices(productsJSON)
+      // remove the leading $ sign for price sorting
+      itemPrices = itemPrices.map(x => x.replace(/\$/g, ''))
+
+      // expected order
+      itemPrices.sort((a,b) => a - b)
+
+      // sort action
       productPage.sortByPriceLH()
+
+      // verify the prices order
+      verifyPricesOrder(itemPrices)
     })
 
-    it('sort by Price (high to low)', () => {
+    it('can sort by Price (high to low)', async () => {
+      const productsJSON = await cy.get('@productsJSON') 
+      let itemPrices = await getItemPrices(productsJSON)
+      // remove the leading $ sign for price sorting
+      itemPrices = itemPrices.map(x => x.replace(/\$/g, ''))
+
+      // expected order
+      itemPrices.sort((a,b) => b - a)
+
+      // sort action
       productPage.sortByPriceHL()
-    })
 
+      // verify the prices order
+      verifyPricesOrder(itemPrices)
+    })
   })
 
+  function verifyNamesOrder(itemNames) {
+    for (let i = 0; i < itemNames.length; i++) {
+      cy.get(`:nth-child(${i+1}) > .inventory_item_description`)
+      .find('.inventory_item_name')
+      .should('have.text', itemNames[i])
+    }
+  }
+
+  async function getItemNames(productsJSON) {
+    let itemNames = []
+    for (let el of productsJSON) {
+      itemNames.push(el.inventory_item_name)
+    }
+
+    return itemNames
+  }
+
+  async function getItemPrices(productsJSON) {
+    let itemPrices = []
+    for (let el of productsJSON) {
+      itemPrices.push(el.inventory_item_price)
+    }
+
+    return itemPrices
+  }
+
+  function verifyPricesOrder(itemPrices) {
+    for (let i = 0; i < itemPrices.length; i++) {
+      cy.get(`:nth-child(${i+1}) > .inventory_item_description`)
+        .find('.inventory_item_price')
+        .should('have.text', `$${itemPrices[i]}`)
+    }
+  }
 })
